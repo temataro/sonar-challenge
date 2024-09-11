3b3acdfe1401d12aa15e35b16d071f22f8236049
Temesgen Ataro <temesgen.ataro@tii.ae>
2024-09-11 15:19:23 +0400

    Read files from data folder

--- get_wavs.sh
+++ get_wavs.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 wget https://acousticstoday.org/wp-content/uploads/2024/03/ASA-Student-Challenge-2024.zip
+mkdir -p ./data
 unzip ASA-Student-Challenge-2024 -d data
 rm ASA-Student-Challenge-2024.zip
