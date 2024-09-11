80e5467a007328b6b451e58d4628ae2c8e5f50ef
Temesgen Ataro <temesgen.ataro@tii.ae>
2024-09-11 14:35:42 +0400

    Download and extract sonar data from website

---
+++ get_wavs.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+wget https://acousticstoday.org/wp-content/uploads/2024/03/ASA-Student-Challenge-2024.zip
+unzip ASA-Student-Challenge-2024 -d data
+rm ASA-Student-Challenge-2024.zip
