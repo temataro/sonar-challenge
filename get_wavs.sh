57b0397c80b17e8c324d34cb66f54a475095f243
Temesgen Ataro <temesgen.ataro@tii.ae>
2024-09-20 10:38:31 +0400

    Add description to shell script that downloads data

--- get_wavs.sh
+++ get_wavs.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Download the hydrophone recordings from the website listed on the challenge document.
+
+echo "Download the hydrophone recordings from the website listed on the challenge document."
 wget https://acousticstoday.org/wp-content/uploads/2024/03/ASA-Student-Challenge-2024.zip
 mkdir -p ./data
 unzip ASA-Student-Challenge-2024 -d data
